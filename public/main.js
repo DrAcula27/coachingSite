1b3bf35e72d94d9cf139b04106cbda4488a994f9
Danielle <danielle@kendallspace.net>
2025-01-14 14:30:16 -0800

    initial commit

---
+++ public/main.js
@@ -0,0 +1,6 @@
+document
+  .querySelector('form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the form from submitting
+    alert('Your message has been sent!');
+  });
