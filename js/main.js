0d4a88635febbb1dd764a5c5f35adc30c16344a3
Danielle <danielle@kendallspace.net>
2025-01-16 17:13:09 -0800

    animate hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-document
-  .querySelector('form')
-  .addEventListener('submit', function (event) {
-    event.preventDefault();
-    alert('Your message has been sent!');
+const navToggleBtn = document.querySelector('#hamburgerBtn');
+const navLinks = document.querySelector('.nav-links');
+const navbar = document.querySelector('#nav');
+
+if (navToggleBtn && navLinks) {
+  navToggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    navbar.classList.toggle('aln-fs');
   });
+}
